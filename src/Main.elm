f2c6a16db371ef4ef0b5c38728677e8620e29880
Andreas Møller <andreas.moller@gmail.com>
2016-11-14 13:23:22 +0000

    render list of speakers

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,15 @@
 import Html exposing (..)
-import Html.Attributes exposing (..)
+import Html.Attributes exposing (class, for, id, action, type_, style)
 type Msg = NoOp
 
+type alias Speaker = {
+  name : String,
+  bio : String
+  }
+
 type alias Model = {
-  title : String
+  title : String,
+  speakers : List Speaker
   }
 
 main : Program Never Model Msg
@@ -14,30 +20,65 @@ main = Html.beginnerProgram {
   }
 
 defaultModel : Model
-defaultModel = {
-  title = "Lnug Search"
+defaultModel =
+  {
+    title = "Lnug Search",
+    speakers =
+      [
+        {
+          name = "Andreas Moller",
+          bio =
+            """
+            Bryan Cranston played the role of Walter in Breaking Bad. He is also known
+            for playing Hal in Malcom in the Middle
+            """
+        },
+        {
+          name = "Stefano Vozza",
+          bio =
+            """
+            Bryan Cranston played the role of Walter in Breaking Bad. He is also known
+            for playing Hal in Malcom in the Middle
+            """
+        },
+        {
+          name = "James Chow",
+          bio =
+            """
+            Bryan Cranston played the role of Walter in Breaking Bad. He is also known
+            for playing Hal in Malcom in the Middle
+            """
+        }
+      ]
   }
 
 view : Model -> Html Msg
-view {title} =
+view {title, speakers} =
   div [] [
-    div [class "mdl-layout__header"] [
+    header [class "mdl-layout__header"] [
       div [class "mdl-layout__header-row"] [
         span [class "mdl-layout-title"] [text title]
       ]
     ],
-    div [class "mdl-cell--12-col"] [
-      ul [class "mdl-list"] [
-        li [class "mld-list__item"] [
-          span [] [text "Andreas Moeller"]
-        ],
-        li [class "mld-list__item"] [
-          span [] [text "Stefano Vozza"]
-        ],
-        li [class "mld-list__item"] [
-          span [] [text "James Chow"]
+    div [class "mdl-grid"] [
+      form [action "#", class "mdl-grid", style [("width", "80%")]] [
+        span [] [text "Search..."],
+        div [class "mdl-textfield mdl-js-textfield block"] [
+          input [class "mdl-textfield__input", type_ "text", id "Search"] []
         ]
-      ]
+      ],
+      ul [class "mdl-list", style [("display", "block"), ("width", "100%")]] <| List.map speaker speakers
+    ]
+  ]
+
+
+speaker : Speaker -> Html Msg
+speaker {name, bio} =
+  li [class "mdl-list__item mdl-list__item--three-line"] [
+    span [class "mdl-list__item-primary-content"][
+      i [class "material-icons mdl-list__item-avatar"] [text "person"],
+      span [] [text name],
+      span [class "mdl-list__item-text-body"] [text bio]
     ]
   ]
 
